4cbbaafa1cde62d2cad205233ce0f1d3bc282b8a
Mila Vizelka <vimila@inbox.ru>
2022-10-20 21:27:05 +0300

    added express server+parcel build

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = 3000;
+app.set("view engine", "pug");
+
+app.use(express.static(__dirname + "/views"));
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Main page" });
+});
+app.get("/reg", (req, res) => {
+  res.render("reg-page", { title: "Reg page" });
+});
+app.get("/sign-in", (req, res) => {
+  res.render("sign-in-page", { title: "Sigh-in page" });
+});
+app.get("/error-404", (req, res) => {
+  res.render("error-404-page", { title: "Error 404 page" });
+});
+app.get("/error", (req, res) => {
+  res.render("error-page", { title: "Error page" });
+});
+app.listen(PORT, console.log("success"));
