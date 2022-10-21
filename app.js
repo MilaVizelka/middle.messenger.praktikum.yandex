d05d11c4311ed380a3c30736c9c340c34f8e687c
Mila Vizelka <vimila@inbox.ru>
2022-10-21 23:06:06 +0300

    созданы страницы

--- app.js
+++ app.js
@@ -5,27 +5,11 @@ const PORT = 3000;
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
+
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
   res.render("index", { title: "Home" });
 });
 
-// app.use(express.static(path.join(__dirname, '/dist')));
-
-// app.get("/", (req, res) => {
-//   res.render("index", { title: "Main page" });
-// });
-// app.get("/reg", (req, res) => {
-//   res.render("reg-page", { title: "Reg page" });
-// });
-// app.get("/sign-in", (req, res) => {
-//   res.render("sign-in-page", { title: "Sigh-in page" });
-// });
-// app.get("/error-404", (req, res) => {
-//   res.render("error-404-page", { title: "Error 404 page" });
-// });
-// app.get("/error", (req, res) => {
-//   res.render("error-page", { title: "Error page" });
-// });
 app.listen(PORT, console.log("success"));
