4f8d402016ad4487ce3b5a98affe54101561d979
Mila Vizelka <vimila@inbox.ru>
2022-10-22 01:56:09 +0300

    fix routing

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const app = express();
 
 app.use(express.static('assets'))
 
-// app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"));
 
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-  res.render("index.pug", { title: "Home" });
+  res.render("index", { title: "Home" });
 });
 
 app.listen(PORT, console.log("success"));
