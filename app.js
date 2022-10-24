6412a84b91dd2277f1a0287486a754bfff32c6b8
Mila Vizelka <vimila@inbox.ru>
2022-10-24 17:17:19 +0300

    настроила раздачу html файла из папки dist

--- app.js
+++ app.js
@@ -4,16 +4,12 @@ const path = require("path");
 const PORT = 3000;
 const app = express();
 
-app.use("/assets", express.static(path.join(__dirname, "assets")));
-
-app.use("/static", express.static(path.join(__dirname, "static")));
-
-app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-  res.render("index", { title: "Home" });
+  res.render("index.html", { title: "Home" });
 });
 
-app.listen(PORT, console.log("success"));
+app.listen(PORT);
