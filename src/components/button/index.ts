c93f2e7bbb73fee53e93a0d5684fc9e347e8244d
Mila Vizelka <l.viselka@vk.teams>
2023-12-12 02:43:40 +0300

    fixed tests

--- src/components/button/index.ts
+++ src/components/button/index.ts
@@ -1,11 +1,9 @@
 import {Block} from '../../utils/Block.ts';
 import {ButtonProps} from '../../models/project.model.ts';
-
 export class Button extends Block {
-    constructor(props: ButtonProps) {
+    constructor({props}: { props: ButtonProps }) {
         super('span', props);
     }
-    
     render(): DocumentFragment {
         return this.compile(`
              <button class="button-styled" type="submit" >{{{ text }}}</button>
