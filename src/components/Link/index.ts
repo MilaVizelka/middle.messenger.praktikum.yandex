0f288280ba0e7c3ad2ae79e4ff6dfd7c5551b560
Mila Vizelka <l.viselka@vk.teams>
2024-01-14 21:19:52 +0300

    обновлен Link

--- src/components/Link/index.ts
+++ src/components/Link/index.ts
@@ -1,14 +1,31 @@
-import {LinkProps} from '../../models/project.model.ts';
 import {Block} from '../../utils/Block.ts';
+import {PropsWithRouter} from "../../hocs/withRouter.ts";
 
+interface LinkProps extends PropsWithRouter {
+    to: string;
+    content: string;
+    events?: {
+        click: () => void;
+    };
+    
+}
 export class Link extends Block {
     constructor(props: LinkProps) {
-        super('div', props);
+        super('div',{
+            ...props,
+            events: {
+                click: () => this.navigate()
+            },
+        });
+    }
+    
+    navigate() {
+        this.props.router.go(this.props.to);
     }
     
     render(): DocumentFragment {
         return this.compile(`
-            <a class = "link-styled" href={{ to }}>{{{ content }}}</a>
+            <span class = "link-styled">{{{ content }}}</span>
         `, this.props)
     }
 }
